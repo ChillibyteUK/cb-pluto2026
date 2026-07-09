87a6d3bde28bc345733ff126a4fcf1054200c88d
ChillibyteUK <dan@chillibyte.co.uk>
2026-07-09 15:59:34 +0100

    mobile tweaks to nav

--- header.php
+++ header.php
@@ -148,7 +148,7 @@ if ( session_status() === PHP_SESSION_NONE ) {
 						array(
 							'theme_location' => $the_menu,
 							'container'      => false,
-							'menu_class'     => 'navbar-nav w-100 justify-content-start gap-5',
+							'menu_class'     => 'navbar-nav w-100 justify-content-start gap-3 gap-md-5 pt-4 pt-md-0',
 							'fallback_cb'    => '',
 							'depth'          => 3,
 							'walker'         => new Understrap_WP_Bootstrap_Navwalker(),
