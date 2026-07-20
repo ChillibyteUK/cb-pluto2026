0d94cfd793b70e59b62d8c0c7e2b1e4d0f8b5026
ChillibyteUK <dan@chillibyte.co.uk>
2026-07-20 14:34:15 +0100

    pushthrough

--- blocks/cb-report-pushthrough.php
+++ blocks/cb-report-pushthrough.php
@@ -33,7 +33,7 @@ $block_uid = 'text-image-' . uniqid();
 <section id="<?= esc_attr( $block_uid ); ?>" class="<?= esc_attr( trim( 'cb-report-pushthrough ' . $bg . ' ' . $fg . ' ' . $classes ) ); ?>">
 	<div class="container">
 		<div class="cb-report-pushthrough__inner" data-aos="fade">
-			<div class="row">
+			<div class="row g-4">
 				<div class="col-md-3">
 					<?php
 					if ( get_field( 'cover' ) ) {
@@ -43,20 +43,31 @@ $block_uid = 'text-image-' . uniqid();
 					?>
 				</div>
 				<div class="col-md-9">
+					<?php
+					if ( get_field( 'title' ) ) {
+						?>
 					<h2 class="pb-4"><?= esc_html( get_field( 'title' ) ); ?></h2>
+						<?php
+					}
+					?>
 					<div class="cb-report-pushthrough__content w-constrained">
 						<?= wp_kses_post( get_field( 'intro' ) ); ?>
 					</div>
-					<div class="cb-report-pushthrough__button">
-						<?php
-						$button = get_field( 'button' );
-						if ( $button ) :
-							$button_url    = $button['url'];
-							$button_title  = $button['title'];
-							$button_target = $button['target'] ? $button['target'] : '_self';
+					<div class="cb-report-pushthrough__cta">
+						<?= wp_kses_post( get_field( 'cta_content' ) ); ?>
+						<div class="cb-report-pushthrough__button">
+							<?php
+							$button = get_field( 'button' );
+							if ( $button ) {
+								$button_url    = $button['url'];
+								$button_title  = $button['title'];
+								$button_target = $button['target'] ? $button['target'] : '_self';
+								?>
+							<a class="cb-link-dot" href="<?= esc_url( $button_url ); ?>" target="<?= esc_attr( $button_target ); ?>"><?= esc_html( $button_title ); ?></a>
+								<?php
+							}
 							?>
-							<a class="button button--inverse" href="<?= esc_url( $button_url ); ?>" target="<?= esc_attr( $button_target ); ?>"><?= esc_html( $button_title ); ?></a>
-						<?php endif; ?>
+						</div>
 					</div>
 				</div>
 			</div>
